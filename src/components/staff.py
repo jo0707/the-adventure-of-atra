828f31b9ec87e85b9381f5761c5f3bd3280a7e3a
xa-4104 <allfajar112234@gmail.com>
2024-05-06 15:06:46 +0700

    Add Staff in Front Lobby

--- src/components/staff.py
+++ src/components/staff.py
@@ -3,5 +3,5 @@ from src.components.gameEntity import GameEntity
 
 
 class Staff(GameEntity):
-    def __init__(self, x, y):
-        super().__init__("assets/images/char/staff/down/0.png", x, y, 0, 0, 1)
+    def __init__(self, x, y, direction):
+        super().__init__(f"assets/images/char/staff/{direction}/0.png", x, y, 0, 0, 1)
--- src/scene/lobbyFrontScene.py
+++ src/scene/lobbyFrontScene.py
@@ -3,6 +3,8 @@ import pygame
 from src.components.frontLeftTable import FrontLeftTable
 from src.components.frontRightTable import FrontRightTable
 from src.components.atra import Atra
+from src.components.wallText import WallText
+from src.components.staff import Staff
 from src.scene.scene import Scene
 from src.utils.screenHelper import ScreenHelper
 from src.utils.eventHelper import EventHelper
@@ -13,18 +15,17 @@ class LobbyFrontScene(Scene):
         self.background = pygame.image.load("assets/images/backgrounds/lobbyFront.png")
         self.background = pygame.transform.scale(self.background, (pygame.display.get_window_size()))
 
-        self.walltext1 = pygame.image.load("assets/images/components/wallText.png")
-        self.walltext2 = pygame.image.load("assets/images/components/wallText.png")
-        self.walltext1_pos = (330, 70)
-        self.walltext2_pos = (845, 70)
+        self.walltext1= WallText(330, 70)
+        self.walltext2= WallText(845, 70)
 
-        
-        
+        self.staff1 = Staff(200, 550, "up")
+        self.staff2 = Staff(972, 550, "left")
+
         self.atra = Atra()
         self.frontLeftTable = FrontLeftTable(0, ScreenHelper.getWindowY())
         self.frontRightTable = FrontRightTable(ScreenHelper.getWindowX(), ScreenHelper.getWindowY())
         self.atra.rect.bottomleft = (ScreenHelper.getWindowX() / 2, ScreenHelper.getWindowY())
-        self.sprites.add(self.atra, self.frontLeftTable, self.frontRightTable)
+        self.sprites.add(self.atra, self.frontLeftTable, self.frontRightTable, self.walltext1, self.walltext2, self.staff1, self.staff2)
     
     def onKeyDown(self, keys):
         self.atra.onKeyDown(keys)
@@ -37,8 +38,6 @@ class LobbyFrontScene(Scene):
     
     def display(self):
         self.screen.blit(self.background, (0, 0))
-        self.screen.blit(self.walltext1, self.walltext1_pos)
-        self.screen.blit(self.walltext2, self.walltext2_pos)
         self.sprites.draw(self.screen)
     
     def update(self):
